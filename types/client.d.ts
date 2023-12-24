d0fcc21113dd05222e1bd70677fb1a7de7b2c2b3
Justin Moser <jmmoser15@gmail.com>
2023-12-24 08:28:10 -0600

    include types

---
+++ types/client.d.ts
@@ -0,0 +1,88 @@
+/**
+ * @param {{
+ *   onSend: (message: Uint8Array, port: number, address: string, broadcast: boolean) => void;
+ *   onDevice?: (device: Device) => void;
+ *   defaultTimeoutMs?: number;
+ * }} options
+ */
+export function Client(options: {
+    onSend: (message: Uint8Array, port: number, address: string, broadcast: boolean) => void;
+    onDevice?: (device: Device) => void;
+    defaultTimeoutMs?: number;
+}): {
+    readonly devices: Map<string, Device>;
+    /**
+     * @template T
+     * @param {import('./commands.js').Command<T>} command
+     */
+    broadcast<T>(command: import("./commands.js").Command<T>): void;
+    /**
+     * @template T
+     * @param {import('./commands.js').Command<T>} command
+     * @param {Device} device
+     */
+    sendWithoutResponse<T_1>(command: import("./commands.js").Command<T_1>, device: Device): void;
+    /**
+     * @template T
+     * @param {import('./commands.js').Command<T>} command
+     * @param {Device} device
+     * @param {AbortSignal} [signal]
+     * @returns {Promise<T>}
+     */
+    send<T_2>(command: import("./commands.js").Command<T_2>, device: Device, signal?: AbortSignal): Promise<T_2>;
+    /**
+     * @param {Device} device
+     */
+    refreshDeviceInfo(device: Device): Promise<void>;
+    /**
+     * @param {string} serialNumber
+     */
+    getDevice(serialNumber: string): any;
+    /**
+     * @param {Uint8Array} message
+     * @param {number} port
+     * @param {string} address
+     */
+    onReceived(message: Uint8Array, port: number, address: string): {
+        header: {
+            buffer: ArrayBuffer;
+            size: number;
+            protocol: number;
+            addressable: boolean;
+            tagged: boolean;
+            origin: number;
+            source: number;
+            target: Uint8Array;
+            reservedTarget2: Uint8Array;
+            reveredSiteMacAddress: Uint8Array;
+            resRequired: boolean;
+            ackRequired: boolean;
+            sequence: number;
+            reserved_timestamp: {
+                buffer: Uint8Array;
+                decoded: any;
+            };
+            reservedProtocolHeader2: Uint8Array;
+            type: number;
+        };
+        payload: any;
+    };
+};
+export type DeviceType = 'light' | 'switch';
+export type Device = {
+    address: string;
+    port: number;
+    target: Uint8Array;
+    serialNumber: string;
+    label?: string;
+    group?: ReturnType<typeof decodeStateGroup>;
+    type?: DeviceType;
+    source: number;
+    color?: ReturnType<typeof decodeLightState>;
+    version?: ReturnType<typeof decodeStateVersion>;
+    hostFirmware?: ReturnType<typeof decodeStateHostFirmware>;
+};
+import { decodeStateGroup } from './encoding.js';
+import { decodeLightState } from './encoding.js';
+import { decodeStateVersion } from './encoding.js';
+import { decodeStateHostFirmware } from './encoding.js';
---
+++ types/index.d.ts
@@ -0,0 +1,4 @@
+export * from "./client.js";
+export * from "./commands.js";
+export * from "./encoding.js";
+export { hsbToRgb, rgbToHsb } from "./utils.js";
